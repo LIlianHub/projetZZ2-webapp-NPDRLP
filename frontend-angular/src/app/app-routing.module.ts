48bfa8ed99da8cd770e275cdefec2bea07fdcc6a
LILianHUb <lilian.ballejos@hotmail.fr>
2022-11-23 13:44:23 +0100

    user front

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -3,18 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ParoleComponent } from './parole/parole.component';
 import { MentionComponent } from './mention/mention.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: '', component:  AccueilComponent},
-  { path: 'accueil', component: AccueilComponent},
-  { path: 'mention', component:  MentionComponent},
-  { path: 'paroles/:artiste/:musique/:difficulte', component:  ParoleComponent},
-  { path: '**', component: AccueilComponent},
+  { path: '', component: AccueilComponent },
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'mention', component: MentionComponent },
+  { path: 'paroles/:artiste/:musique/:difficulte', component: ParoleComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', component: AccueilComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ frontend-angular/src/app/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { TokenStorageService } from '../services/token-storage.service';
+import { Router } from '@angular/router';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  FormData!: FormGroup;
+  errorMessage = '';
+
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService,
+    private router: Router,
+    private builder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.goHome();
+    }
+    //si pas de token on affiche le formulaire de login
+    this.FormData = this.builder.group({
+      Login: new FormControl('', [Validators.required]),
+      Password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit(): void {
+    this.authService
+      .login(this.FormData.value.Login, this.FormData.value.Password)
+      .subscribe(
+        (data) => {
+          this.tokenStorage.saveToken(data.accessToken);
+          this.tokenStorage.saveUser(data);
+          this.goHome();
+        },
+        (err) => {
+          console.log(err);
+          this.FormData.reset();
+          //mettre message d'erreur dans erroMessage
+        }
+      );
+  }
+
+  goHome(): void {
+    this.router.navigate(['/profile']);
+  }
+}
