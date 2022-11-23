2e5ffca07ff73efbd84d731d9adb1c3cb33ba93f
Etienne Charpin <etiennecharpin@gmail.com>
2022-11-23 13:44:04 +0100

    ajout creation connexion et insertion db

--- backend-api/lib/gestion_database.js
+++ backend-api/lib/gestion_database.js
@@ -1,1 +1,56 @@
-//base de donnée
+var mysql = require('mysql'); 
+
+
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootsqlpsw",
+    database: "mydb"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+
+
+    /*CREATE TABLE
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });*/
+
+
+
+    /* DROP TABLE
+    var sql = "DROP TABLE customers";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table deleted");
+    });*/
+
+   
+   
+    /*INSERT VALUE
+    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });*/
+
+
+    /*SELECT
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+    */
+
+
+
+
+  }); 
+
+
+
