c60987f026bc9e6f85a611ae4c06d6698d656c05
LILianHUb <lilian.ballejos@hotmail.fr>
2022-11-23 22:47:40 +0100

    token

--- backend-api/lib/gestion_user.js
+++ backend-api/lib/gestion_user.js
@@ -1,17 +1,21 @@
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const fs = require("fs");
+
+// en general pour le lire au démarage du serveur
+const encodedToken = fs.readFileSync("./data/encoded-token", "utf8");
 
 function register(username, password) {
   //appel base de donnée pour vérifier si le username existe déjà (fonction existUser() dans gestion database)
-    //si oui, renvoyer un message d'erreur
+  //si oui, renvoyer un message d'erreur
 
-    //si non
+  //si non
   let hashedPassword = bcrypt.hashSync(password, 8);
   //ajout dans base de donnée
-
 }
 
-function login(username, password) {
+async function login(username, password) {
+  return new Promise((resolve, reject) => {
     //on essaye de récupérer l'utilisateur dans la base de donnée
     //si on ne le trouve pas, on renvoie un message d'erreur
     //si il existe, on vérifie le mot de passe
@@ -19,4 +23,27 @@ function login(username, password) {
     //si le mot de passe est bon, on renvoie un token et le username
 
     //sinon on renvoie un message d'erreur
+
+  });
+}
+
+//let token = req.headers["x-access-token"];
+
+async function verifyToken(token) {
+  return new Promise((resolve, reject) => {
+    if (!token) {
+      reject("Pas de token fourni");
+    }
+
+    jwt.verify(token, encodedToken, (err, decoded) => {
+      if (err) {
+        reject("Token invalide");
+      }
+      resolve(decoded.id);
+    });
+  });
+}
+
+async function register(username, password) {
+  return new Promise((resolve, reject) => {});
 }
