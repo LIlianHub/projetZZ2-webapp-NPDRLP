e01d77a300eeb4902a23d5fdcadf7d52e97f76ca
Etienne Charpin <etiennecharpin@gmail.com>
2022-11-27 22:48:49 +0100

    modif database

--- backend-api/lib/gestion_database.js
+++ backend-api/lib/gestion_database.js
@@ -4,7 +4,8 @@ const fs = require("fs");
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: fs.readFileSync("./data/data-base", "utf8"),
+  password : "rootsqlpsw",
+  //password: fs.readFileSync("./data/data-base", "utf8"),
   database: "mydb",
 });
 
@@ -58,6 +59,82 @@ function insertUser(username, pswd) {
     if (err) throw err;
   });
 }
+
+
+function addFolder(foldername, username) {
+  var sql = 'INSERT INTO folder VALUES ("' + foldername + '","' + username + '")';
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+  });
+}
+
+function insertMusicIntoFolder(idMusic, artiste, title, idFolder) {
+  var sql = 'INSERT INTO musics VALUES ("' + idMusic + '","' + artiste + '","' + title + '","' + idFolder + '")';
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+  });
+}
+
+
+async function getUserFolders(username) {
+
+  let nb = await getUserNBFolders(username);
+
+  return new Promise(async (resolve, reject) => {
+    let folderTab = [];
+    console.log("here");
+    con.query(
+      'SELECT idFOLDER FROM folder WHERE USERNAME = "' + username + '"',
+      function (err, result, fields) {
+        if (err) reject(err);
+        console.log("ici");
+        console.log(result);
+        console.log(nb);
+        for(let i = 0;i < nb ; i++){
+          folderTab[i] = result[i].idFOLDER;
+          console.log(folderTab[i]);
+        }
+        resolve(folderTab);
+      }
+    );
+  });
+}
+
+
+async function getUserNBFolders(username) {
+  return new Promise(async (resolve, reject) => {
+    con.query(
+      'SELECT count(*) nb FROM folder WHERE USERNAME = "' + username + '"',
+      function (err, result, fields) {
+        //console.log(result[1].idFOLDER);
+        if (err) reject(err);
+
+        console.log(result[0].nb);
+        resolve(result[0].nb);
+      }
+    );
+  });
+}
+
+
+async function getUserFoldersAndMusics(username) {
+
+  let foldersIDS = await getUserFolders(username);
+
+  return new Promise(async (resolve, reject) => {
+    con.query(
+      'SELECT idmusics, artist, title FROM musics WHERE foldernum = 1',
+      function (err, result, fields) {
+        console.log(result);
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+}
+
+//getUserFoldersAndMusics("test");
+
 
 module.exports = { getUserInfo, insertUser, alreadyUser, connectionDataBase };
 
